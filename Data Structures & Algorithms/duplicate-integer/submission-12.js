38bac4987b3cf1314a9428b2b413d8fca096ff77
Nathan Xie <81936161+na3th9n@users.noreply.github.com>
2026-06-24 11:13:49 -0400

    Add: duplicate-integer - submission-12

---
+++ Data Structures & Algorithms/duplicate-integer/submission-12.js
@@ -0,0 +1,20 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {boolean}
+     */
+    hasDuplicate(nums) {
+        if (nums.length <= 1) {
+            return false
+        }
+
+        nums.sort((a, b) => a - b);
+
+        for(let i = 0; i < nums.length-1; i++) {
+            if (nums[i] === nums[i+1]) {
+                return true
+            }
+        }
+        return false
+    }
+}
