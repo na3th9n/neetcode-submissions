4e43ac4650bb107189cf6d547acffc88ab4499b2
Nathan Xie <81936161+na3th9n@users.noreply.github.com>
2026-07-01 15:38:37 -0400

    Add: longest-substring-without-duplicates - submission-9

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-9.js
@@ -0,0 +1,31 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {number}
+     */
+    lengthOfLongestSubstring(s) {
+        if (s.length === 0) {
+            return 0;
+        } 
+
+        if (s.length === 1) {
+            return 1
+        }
+
+        const seen = new Set();
+        let l = 0;
+        let res = 1;
+        seen.add(s[l]);
+
+        for (let r = 1; r < s.length; r++) {
+            while (seen.has(s[r])) {
+                seen.delete(s[l]);
+                l++;
+            }
+
+            seen.add(s[r]);
+            res = Math.max(res, r - l + 1);
+        }
+        return res;
+    }
+}
