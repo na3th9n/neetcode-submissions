8df45e6825dbd3d46233d6665baeea8c6050da9f
Nathan Xie <81936161+na3th9n@users.noreply.github.com>
2026-07-10 09:19:12 -0400

    Add: max-area-of-island - submission-3

---
+++ Data Structures & Algorithms/max-area-of-island/submission-3.js
@@ -0,0 +1,54 @@
+class Solution {
+    /**
+     * @param {number[][]} grid
+     * @return {number}
+     */
+    maxAreaOfIsland(grid) {
+        // approach #1: bfs
+        // when encountering a 1, run bfs to find the total area, so everytime there is a 1 connected
+        // horizontally or vertically, add to a temporary variable
+        // once bfs is over, compare to a global variable using the Math.max() function
+        // remember that we have to turn a cell to "0" to indicate that it has been visited 
+
+        let maxArea = 0;
+        const ROWS = grid.length;
+        const COLS = grid[0].length;
+        const q = new Queue();
+        const DIRS = [[-1, 0], [1, 0], [0, -1], [0, 1]];
+
+        const bfs = (r, c) => {
+            let area = 0;
+
+            q.enqueue([r, c]);
+            area++;
+            grid[r][c] = 0;
+
+            while (!q.isEmpty()) {
+                const [cr, cc] = q.dequeue();
+
+                for (const [dr, dc] of DIRS) {
+                    const nr = cr + dr;
+                    const nc = cc + dc;
+
+                    if (nr >= 0 && nr < ROWS && nc >= 0 && nc < COLS && grid[nr][nc] === 1) {
+                        q.enqueue([nr, nc]);
+                        grid[nr][nc] = 0;
+                        area++;
+                    }
+                }
+            }
+
+            return area;
+        }
+        
+        for (let r = 0; r < ROWS; r++) {
+            for (let c = 0; c < COLS; c++) {
+                if (grid[r][c] === 1) {
+                    maxArea = Math.max(bfs(r, c), maxArea);
+                }
+            }
+        }
+
+        return maxArea;
+    }
+}
