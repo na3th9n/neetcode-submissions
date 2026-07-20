bbd8a2770ef0e4a439deda44a69bfb4f908ebf0b
Nathan Xie <81936161+na3th9n@users.noreply.github.com>
2026-07-20 17:13:19 -0400

    Add: climbing-stairs - submission-1

---
+++ Data Structures & Algorithms/climbing-stairs/submission-1.js
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {number} n
+     * @return {number}
+     */
+    climbStairs(n) {
+        if (n === 1) {
+            return 1
+        }
+
+        const memo = new Array(n + 1);
+        memo[0] = 1;
+        memo[1] = 1;
+
+        for (let i = 2; i <= n; i++) {
+            memo[i] = memo[i - 1] + memo[i - 2];
+        }   
+
+        return memo[n];
+    }
+}
